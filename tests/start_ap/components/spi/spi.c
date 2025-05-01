2b9a90bd73e8de9ce52d9f45fb13a8b1b3af7f8a
clrvyntx <73581070+clrvyntx@users.noreply.github.com>
2025-04-30 23:29:46 -0300

    Add files via upload

---
+++ tests/start_ap/components/spi/spi.c
@@ -0,0 +1,98 @@
+#include "spi.h"
+#include "esp_log.h"
+#include "ring_link_payload.h"
+
+
+static spi_device_handle_t s_spi_device_handle = {0};
+static const char* TAG = "==> SPI";
+
+
+static esp_err_t spi_rx_init() {
+    //Configuration for the SPI bus
+    spi_bus_config_t buscfg={
+        .mosi_io_num=SPI_RECEIVER_GPIO_MOSI,
+        .miso_io_num=-1,
+        .sclk_io_num=SPI_RECEIVER_GPIO_SCLK,
+        .quadwp_io_num = -1,
+        .quadhd_io_num = -1,
+    };
+
+    //Configuration for the SPI slave interface
+    spi_slave_interface_config_t slvcfg={
+        .mode=0,
+        .spics_io_num=SPI_RECEIVER_GPIO_CS,
+        .queue_size=SPI_QUEUE_SIZE,
+        .flags=0,
+        //.post_setup_cb=spi_post_setup_cb,
+        //.post_trans_cb=spi_post_trans_cb
+    };
+
+    //Initialize SPI slave interface
+    ESP_ERROR_CHECK(spi_slave_initialize(SPI_RECEIVER_HOST, &buscfg, &slvcfg, SPI_DMA_CH_AUTO));
+    return ESP_OK;
+}
+
+static esp_err_t spi_tx_init() {
+    //Configuration for the SPI bus
+    spi_bus_config_t buscfg={
+        .mosi_io_num=SPI_SENDER_GPIO_MOSI,
+        .miso_io_num=-1,
+        .sclk_io_num=SPI_SENDER_GPIO_SCLK,
+        .quadwp_io_num=-1,
+        .quadhd_io_num=-1
+    };
+
+    //Configuration for the SPI device on the other side of the bus
+    spi_device_interface_config_t devcfg={
+        .command_bits=0,
+        .address_bits=0,
+        .dummy_bits=0,
+        .clock_speed_hz=SPI_FREQ,
+        .duty_cycle_pos=128,        //50% duty cycle
+        .mode=0,
+        .spics_io_num=SPI_SENDER_GPIO_CS,
+        .cs_ena_pretrans = 3,   
+        .cs_ena_posttrans = 10,        //Keep the CS low 3 cycles after transaction, to stop slave from missing the last bit when CS has less propagation delay than CLK
+        .queue_size=SPI_QUEUE_SIZE
+    };
+
+    //Initialize the SPI bus and add the device we want to send stuff to.
+    ESP_ERROR_CHECK(spi_bus_initialize(SPI_SENDER_HOST, &buscfg, SPI_DMA_CH_AUTO));
+    ESP_ERROR_CHECK(spi_bus_add_device(SPI_SENDER_HOST, &devcfg, &s_spi_device_handle));
+    return ESP_OK;
+}
+
+esp_err_t spi_init(void) {
+    ESP_ERROR_CHECK(spi_rx_init());
+    ESP_ERROR_CHECK(spi_tx_init());
+    return ESP_OK;
+}
+
+esp_err_t spi_transmit(void *p, size_t len) {
+    ring_link_payload_t* payload = (ring_link_payload_t*)p;
+    ESP_LOGI(TAG, "Pre-transmit payload - ID: %d, TTL: %d", payload->id, payload->ttl);
+             
+    spi_transaction_t t = {
+        .length = len * 8,
+        .tx_buffer = p,
+    };
+    return spi_device_transmit(s_spi_device_handle, &t);
+}
+
+esp_err_t spi_receive(void *p, size_t len)
+{
+    spi_slave_transaction_t t = {
+        .rx_buffer = p,
+        .length = len * 8,
+    };
+    esp_err_t ret = spi_slave_transmit(SPI_RECEIVER_HOST, &t, portMAX_DELAY);
+    
+    if (ret == ESP_OK) {
+        // Verificar que realmente recibimos la cantidad correcta de datos
+        ESP_LOGI(TAG, "SPI transaction complete - Received %d bits", t.trans_len);
+        if (t.trans_len != len * 8) {
+            ESP_LOGW(TAG, "Incomplete transaction! Expected %d bits", len * 8);
+        }
+    }
+    return ret;
+}
---
+++ tests/start_ap/main/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "esp_event.h"
+#include "esp_check.h"
+
+#include "device.h"
+
+static const char *TAG = "==> main";
+
+void app_main(void) {
+
+    Device device;
+    Device *device_ptr = &device;
+
+    const char *device_uuid = "ESP-01";
+    const uint8_t device_orientation = 0;
+
+    const char *wifi_network_prefix = "AP_Test";
+    const char *wifi_network_password = "test123456";
+
+    const uint8_t ap_channel_to_emit = 6;
+    const uint8_t ap_max_sta_connections = 4;
+
+    const char *network_cidr = "10.0.0.1";
+    const char *network_gateway = "10.0.0.1";
+    const char *network_mask = "255.0.0.0";
+
+    const uint8_t device_is_root = 1;
+    Device_Mode mode = AP;
+
+    device_init(device_ptr, device_uuid, device_orientation, wifi_network_prefix, wifi_network_password, ap_channel_to_emit, ap_max_sta_connections, device_is_root, mode);
+    device_set_network_ap(device_ptr, network_cidr, network_gateway, network_mask);
+    device_start_ap(device_ptr);
+
+}
