6c73acb9672517312095621c15223bf774ed47a7
clrvyntx <73581070+clrvyntx@users.noreply.github.com>
2025-05-03 18:34:19 -0300

    Update server.h

--- components/wireless/server/server.h
+++ components/wireless/server/server.h
@@ -1,12 +1,18 @@
+#include "esp_event.h"
+#include "esp_log.h"
+#include "esp_netif.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "lwip/err.h"
+#include "lwip/sockets.h"
+#include "lwip/sys.h"
+#include "nvs_flash.h"
+#include "esp_netif.h"
+#include <lwip/netdb.h>
 #include <string.h>
-#include <sys/socket.h>
-#include <netdb.h>
-#include <arpa/inet.h>
-#include <unistd.h>
-#include <errno.h>
-#include <esp_log.h>
-#include <freertos/FreeRTOS.h>
-#include <freertos/task.h>
+#include <sys/param.h>
 
 void create_server();
 
