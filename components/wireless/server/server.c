abfdd514e83b9fa8f1af918139e777cb049bd408
clrvyntx <73581070+clrvyntx@users.noreply.github.com>
2025-05-02 22:57:37 -0300

    Update server.c

--- components/wireless/server/server.c
+++ components/wireless/server/server.c
@@ -39,29 +39,18 @@ static void do_retransmit(const int sock) {
   int len;
   char rx_buffer[512];
 
-  do {
-    len = recv(sock, rx_buffer, sizeof(rx_buffer) - 1, 0);
-    if (len < 0) {
-      ESP_LOGE(LOGGING_TAG, "Error occurred during receiving: errno %d", errno);
-    } else if (len == 0) {
-      ESP_LOGW(LOGGING_TAG, "Connection closed");
-    } else {
-      ESP_LOGI(LOGGING_TAG, "Received %d bytes: %s", len, rx_buffer);
+ len = recv(sock, rx_buffer, sizeof(rx_buffer) - 1, 0);
+ if (len < 0) {
+   ESP_LOGE(LOGGING_TAG, "Error occurred during receiving: errno %d", errno);
+ } else if (len == 0) {
+   ESP_LOGW(LOGGING_TAG, "Connection closed");
+ } else {
+   ESP_LOGI(LOGGING_TAG, "Received %d bytes: %s", len, rx_buffer);
 
-      // send() can return less bytes than supplied length.
-      // Walk-around for robust implementation.
-      int to_write = len;
-      while (to_write > 0) {
-        int written = send(sock, rx_buffer + (len - to_write), to_write, 0);
-        if (written < 0) {
-          ESP_LOGE(LOGGING_TAG, "Error occurred during sending: errno %d", errno);
-          // Failed to retransmit, giving up
-          return;
-        }
-        to_write -= written;
-      }
-    }
-  } while (len > 0);
+   // Process payload here with wireless message handling event 
+
+ }
+
 }
 
 static void tcp_server_task(void *pvParameters) {
