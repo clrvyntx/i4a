0a1453814efce120696420d796191a18cec7262e
clrvyntx <73581070+clrvyntx@users.noreply.github.com>
2025-05-03 17:52:00 -0300

    Update utils.c

--- components/wireless/station/utils.c
+++ components/wireless/station/utils.c
@@ -5,23 +5,24 @@
 static const uint16_t SOUTH_NORTH_CHANNELS[] = {1, 3, 5, 7, 9, 11, 13};
 static const uint16_t EAST_WEST_CHANNELS[] = {2, 4, 6, 8, 10, 12};
 
-bool is_in_list(uint16_t number, const uint16_t list[]) {
-  int size = sizeof(list) / sizeof(list[0]);
+bool is_in_list(uint16_t number, const uint16_t list[], int size) {
   for (int i = 0; i < size; i++) {
-      if (list[i] == number) {
-        return true;
-      }
+    if (list[i] == number) {
+      return true;
+    }
   }
   return false;
 }
 
 bool is_channel_allowed(uint16_t orientation, uint16_t channel) {
   if (orientation == 0 || orientation == 1) {
-    return is_in_list(channel, SOUTH_NORTH_CHANNELS);
+    // Pass the size of the SOUTH_NORTH_CHANNELS array
+    return is_in_list(channel, SOUTH_NORTH_CHANNELS, sizeof(SOUTH_NORTH_CHANNELS) / sizeof(SOUTH_NORTH_CHANNELS[0]));
   } else if (orientation == 2 || orientation == 3) {
-    return is_in_list(channel, EAST_WEST_CHANNELS);
+    // Pass the size of the EAST_WEST_CHANNELS array
+    return is_in_list(channel, EAST_WEST_CHANNELS, sizeof(EAST_WEST_CHANNELS) / sizeof(EAST_WEST_CHANNELS[0]));
   } else {
-    return false; //TODO: levantar exception?
+    return false; // Invalid orientation
   }
 }
 
