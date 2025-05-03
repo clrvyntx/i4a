489808a0c9d6af77c3ec2b409abdad16d28d4ea8
clrvyntx <73581070+clrvyntx@users.noreply.github.com>
2025-05-03 18:07:25 -0300

    Update client.h

--- components/wireless/client/client.h
+++ components/wireless/client/client.h
@@ -1,1 +1,13 @@
-void client(const char* ip);
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
+
+void create_client(void);
+bool send_message(const char *ip, const uint8_t *msg, uint16_t len);
