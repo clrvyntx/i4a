5add78144e8c39176e49149037600ebb24d74a0d
clrvyntx <73581070+clrvyntx@users.noreply.github.com>
2025-05-02 22:59:38 -0300

    Update server.c

--- components/wireless/server/server.c
+++ components/wireless/server/server.c
@@ -1,31 +1,3 @@
-/* BSD Socket API Example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
-
-
-
-#include "esp_event.h"
-#include "esp_log.h"
-#include "esp_netif.h"
-#include "esp_system.h"
-#include "esp_wifi.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "lwip/err.h"
-#include "lwip/sockets.h"
-#include "lwip/sys.h"
-#include "nvs_flash.h"
-#include "esp_netif.h"
-
-#include <lwip/netdb.h>
-#include <string.h>
-#include <sys/param.h>
-
 #include "server.h"
 
 #define PORT CONFIG_EXAMPLE_PORT
