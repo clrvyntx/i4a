2eadb2edd5b9ed1c55e7454bae9d04cd395a9dfb
clrvyntx <73581070+clrvyntx@users.noreply.github.com>
2025-05-03 18:07:11 -0300

    Update client.c

--- components/wireless/client/client.c
+++ components/wireless/client/client.c
@@ -1,87 +1,43 @@
-#include "sdkconfig.h"
-#include <string.h>
-#include <unistd.h>
-#include <sys/socket.h>
-#include <errno.h>
-#include <netdb.h>
-#include <arpa/inet.h>
-#include "esp_netif.h"
-#include "esp_log.h"
 #include "client.h"
-#if defined(CONFIG_EXAMPLE_SOCKET_IP_INPUT_STDIN)
-#include "addr_from_stdin.h"
-#endif
 
-#if defined(CONFIG_EXAMPLE_IPV4)
-#define HOST_IP_ADDR CONFIG_EXAMPLE_IPV4_ADDR
-#elif defined(CONFIG_EXAMPLE_SOCKET_IP_INPUT_STDIN)
-#endif
+#define PORT 3999
+#define BUFFER_SIZE 512
 
-#define PORT CONFIG_EXAMPLE_PORT
+static const char *LOGGING_TAG = "udp_client";
 
-static const char *LOGGING_TAG = "example";
-static const char *payload = "Message from ESP32 ";
+// Global variable for the socket
+static int sock = -1;
 
-
-void client(const char* ip)
-{
-    char rx_buffer[128];
-    char host_ip[] = "192.168.4.1";
-    int addr_family = 0;
-    int ip_protocol = 0;
-
-    while (1) {
-#if defined(CONFIG_EXAMPLE_IPV4)
-        struct sockaddr_in dest_addr;
-        inet_pton(AF_INET, host_ip, &dest_addr.sin_addr);
-        dest_addr.sin_family = AF_INET;
-        dest_addr.sin_port = htons(PORT);
-        addr_family = AF_INET;
-        ip_protocol = IPPROTO_IP;
-#elif defined(CONFIG_EXAMPLE_SOCKET_IP_INPUT_STDIN)
-        struct sockaddr_storage dest_addr = { 0 };
-        ESP_ERROR_CHECK(get_addr_from_stdin(PORT, SOCK_STREAM, &ip_protocol, &addr_family, &dest_addr));
-#endif
-
-        int sock =  socket(addr_family, SOCK_STREAM, ip_protocol);
-        if (sock < 0) {
-            ESP_LOGE(LOGGING_TAG, "Unable to create socket: errno %d", errno);
-            break;
-        }
-        ESP_LOGI(LOGGING_TAG, "Socket created, connecting to %s:%d", host_ip, PORT);
-
-        int err = connect(sock, (struct sockaddr *)&dest_addr, sizeof(dest_addr));
-        if (err != 0) {
-            ESP_LOGE(LOGGING_TAG, "Socket unable to connect: errno %d", errno);
-            break;
-        }
-        ESP_LOGI(LOGGING_TAG, "Successfully connected");
-
-        while (1) {
-            int err = send(sock, payload, strlen(payload), 0);
-            if (err < 0) {
-                ESP_LOGE(LOGGING_TAG, "Error occurred during sending: errno %d", errno);
-                break;
-            }
-
-            int len = recv(sock, rx_buffer, sizeof(rx_buffer) - 1, 0);
-            // Error occurred during receiving
-            if (len < 0) {
-                ESP_LOGE(LOGGING_TAG, "recv failed: errno %d", errno);
-                break;
-            }
-            // Data received
-            else {
-                rx_buffer[len] = 0; // Null-terminate whatever we received and treat like a string
-                ESP_LOGI(LOGGING_TAG, "Received %d bytes from %s:", len, host_ip);
-                ESP_LOGI(LOGGING_TAG, "%s", rx_buffer);
-            }
-        }
-
-        if (sock != -1) {
-            ESP_LOGE(LOGGING_TAG, "Shutting down socket and restarting...");
-            shutdown(sock, 0);
-            close(sock);
-        }
+// Function to send a message to the server
+bool send_message(const char *ip, const uint8_t *msg, uint16_t len) {
+    if (sock < 0) {
+        ESP_LOGE(LOGGING_TAG, "Client socket not created yet");
+        return false;
     }
+
+    struct sockaddr_in dest_addr;
+    dest_addr.sin_family = AF_INET;
+    dest_addr.sin_port = htons(PORT);
+    dest_addr.sin_addr.s_addr = inet_addr(ip);  // Set the server's IP here
+
+    int err = sendto(sock, msg, len, 0, (struct sockaddr *)&dest_addr, sizeof(dest_addr));
+
+    if (err < 0) {
+        ESP_LOGE(LOGGING_TAG, "Send failed: errno %d", errno);
+        return false;
+    } else {
+        ESP_LOGI(LOGGING_TAG, "Sent %d bytes to %s", len, ip);
+        return true;
+    }
+}
+
+// Function to create the client task and initialize the client
+void create_client(void) {
+    sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_IP);
+    if (sock < 0) {
+        ESP_LOGE(LOGGING_TAG, "Unable to create socket: errno %d", errno);
+        return;
+    }
+
+    ESP_LOGI(LOGGING_TAG, "Socket created");
 }
