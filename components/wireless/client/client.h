63956aae6448a26f20e0d537cf7807dff8576918
clrvyntx <73581070+clrvyntx@users.noreply.github.com>
2025-05-05 03:09:00 -0300

    Update client.h

--- components/wireless/client/client.h
+++ components/wireless/client/client.h
@@ -4,7 +4,13 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include "esp_netif.h"
+#include "esp_wifi.h"
 #include "esp_log.h"
+#include "esp_event.h"
+#include "lwip/sockets.h"
+#include "lwip/inet.h"
+#include "lwip/netdb.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
