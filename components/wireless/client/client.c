bd080fdc8f64ecc538e52d5c12c7d89ac6af4849
clrvyntx <73581070+clrvyntx@users.noreply.github.com>
2025-05-04 23:29:59 -0300

    Update client.c

--- components/wireless/client/client.c
+++ components/wireless/client/client.c
@@ -2,13 +2,11 @@
 
 #define SERVER_PORT 3999
 #define BUFFER_SIZE 512
-#define MAX_RETRIES 3
 #define RETRY_DELAY_MS 5000
 
 static const char *LOGGING_TAG = "tcp_client";
 
-static int client_sock = -1;
-static TaskHandle_t tcp_client_task_handle = NULL;
+static int server_sock = -1;
 
 static void socket_read_loop(const int sock, const char *server_ip) {
     uint8_t rx_buffer[BUFFER_SIZE];
@@ -32,77 +30,54 @@ static void tcp_client_task(void *pvParameters) {
     const char *server_ip = (const char *)pvParameters;
     struct sockaddr_in server_addr;
 
-    while (1) {
-        client_sock = socket(AF_INET, SOCK_STREAM, IPPROTO_IP);
-        if (client_sock < 0) {
-            ESP_LOGE(LOGGING_TAG, "Unable to create socket: errno %d", errno);
-            vTaskDelay(RETRY_DELAY_MS / portTICK_PERIOD_MS);  // Wait before retrying
-            continue;  // Retry socket creation
-        }
-
-        server_addr.sin_family = AF_INET;
-        server_addr.sin_port = htons(SERVER_PORT);
-        inet_pton(AF_INET, server_ip, &server_addr.sin_addr);
-
-        ESP_LOGI(LOGGING_TAG, "Connecting to %s:%d...", server_ip, SERVER_PORT);
-
-        int err = connect(client_sock, (struct sockaddr *)&server_addr, sizeof(server_addr));
-        if (err != 0) {
-            ESP_LOGE(LOGGING_TAG, "Socket unable to connect: errno %d", errno);
-            close(client_sock);
-            client_sock = -1;
-            vTaskDelay(RETRY_DELAY_MS / portTICK_PERIOD_MS);  // Wait before retrying
-            continue;  // Retry connection
-        } else {
-            ESP_LOGI(LOGGING_TAG, "Successfully connected to %s", server_ip);
-            // call on_peer_connected()
-            socket_read_loop(client_sock, server_ip);
-            break;  // Exit loop after successful connection
-        }
-    }
-
-    ESP_LOGI(LOGGING_TAG, "Closing connection from %s", server_ip);
-    client_disconnect();
-    // call on_peer_lost();
-
-}
-
-void client_disconnect() {
-    if (client_sock >= 0) {
-        shutdown(client_sock, 0);  // Shutdown the socket
-        close(client_sock);        // Close the socket
-        client_sock = -1;          // Reset the client socket to an invalid state
-        ESP_LOGI(LOGGING_TAG, "Disconnected from server");
-    } else {
-        ESP_LOGW(LOGGING_TAG, "Client is not connected, nothing to disconnect");
-    }
-
-    // If the TCP client task is still running, delete it
-    if (tcp_client_task_handle != NULL) {
-        ESP_LOGI(LOGGING_TAG, "Killing TCP client task.");
-        vTaskDelete(tcp_client_task_handle);
-        tcp_client_task_handle = NULL;  // Reset the task handle
-    }
-}
-
-void client_connect(const char *ip) {
-    // If already connected, disconnect first
-    if (client_sock >= 0) {
-        client_disconnect();
-    }
-
-    if (tcp_client_task_handle != NULL) {
-        ESP_LOGW(LOGGING_TAG, "A connection attempt is already in progress.");
+    sock = socket(AF_INET, SOCK_STREAM, IPPROTO_IP);
+    if (sock < 0) {
+        ESP_LOGE(LOGGING_TAG, "Unable to create socket: errno %d", errno);
+        vTaskDelete(NULL);
         return;
     }
 
-    // Start a new connection task
-    xTaskCreate(tcp_client_task, "tcp_client", 4096, (void *)ip, 5, &tcp_client_task_handle);
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(SERVER_PORT);
+    inet_pton(AF_INET, server_ip, &server_addr.sin_addr);
+
+    ESP_LOGI(LOGGING_TAG, "Connecting to %s:%d...", server_ip, SERVER_PORT);
+
+    while (1) {
+        int err = connect(sock, (struct sockaddr *)&server_addr, sizeof(server_addr));
+        if (err != 0) {
+            ESP_LOGE(LOGGING_TAG, "Socket unable to connect: errno %d, retrying in 5 seconds...", errno);
+            close(sock);
+            vTaskDelay(RETRY_DELAY_MS / portTICK_PERIOD_MS);  // Wait before retrying
+            continue;  // Retry connection
+        } else {
+            break; // Successfully connected
+        }
+    }
+
+    ESP_LOGI(LOGGING_TAG, "Successfully connected to %s", server_ip);
+    server_sock = sock;
+    // call on_peer_connected()
+
+    // Handle incoming data
+    socket_read_loop(server_sock, server_ip);
+
+    // Cleanup once connection has been closed
+    ESP_LOGI(LOGGING_TAG, "Closing connection from %s", server_ip);
+    server_sock = -1;
+    // call on_peer_lost();
+    shutdown(sock, 0);
+    close(sock);
+
+}
+
+void client_connect(const char *ip) {
+    xTaskCreate(tcp_client_task, "tcp_client", 4096, (void *)ip, 5, NULL);
 }
 
 bool client_send_message(const uint8_t *msg, uint16_t len) {
-    if (client_sock < 0) {
-        ESP_LOGW(LOGGING_TAG, "Client not connected");
+    if (server_sock < 0) {
+        ESP_LOGW(LOGGING_TAG, "Not connected to server");
         return false;
     }
 
@@ -111,7 +86,7 @@ bool client_send_message(const uint8_t *msg, uint16_t len) {
         return false;
     }
 
-    int sent = send(client_sock, msg, len, 0);
+    int sent = send(server_sock, msg, len, 0);
     if (sent == len) {
         ESP_LOGI(LOGGING_TAG, "Sent %d bytes to server", sent);
         return true;
