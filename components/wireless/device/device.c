0d481485e1787f4ff34d9fcd6e73fa4d6acecae1
clrvyntx <73581070+clrvyntx@users.noreply.github.com>
2025-05-06 20:53:21 -0300

    Update device.c

--- components/wireless/device/device.c
+++ components/wireless/device/device.c
@@ -1,167 +1,192 @@
-#include <lwip/netdb.h>
-
 #include "esp_event.h"
 #include "esp_log.h"
-#include "esp_wifi.h"
-#include "esp_mac.h"
+#include "esp_event.h"
 #include "esp_netif.h"
+#include "esp_system.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/event_groups.h"
-#include "lwip/err.h"
-#include "lwip/sockets.h"
-#include "lwip/sys.h"
+#include "freertos/task.h"
 #include "nvs_flash.h"
-
-#include "station.h"
 #include <string.h>
-#include "utils.c"
-#include "../client/client.h"
+#include <unistd.h>
 
-#define DEFAULT_SCAN_LIST_SIZE 10
-#define EXAMPLE_ESP_MAXIMUM_RETRY 10
+#include "device.h"
 
-static const char* LOGGING_TAG = "station";
+static const char *LOGGING_TAG = "device";
 
-static int s_retry_num = 0;
+// Function to initialize NVS (non-volatile storage)
+static esp_err_t init_nvs() {
+  esp_err_t ret = nvs_flash_init();
+  if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+    ESP_ERROR_CHECK(nvs_flash_erase());
+    ret = nvs_flash_init();
+  }
+  return ret;
+}
 
-/*
- * @brief Initialize the WiFi stack in station mode
- */
-void station_init(StationPtr stationPtr, const char* wifi_ssid_like, uint16_t orientation, char* device_uuid, const char* password) {
+// Function to initialize the Wi-Fi interface
+esp_err_t wifi_init() {
+  // Initialize NVS
+  esp_err_t ret = init_nvs();
+  if (ret != ESP_OK) {
+    ESP_LOGE(LOGGING_TAG, "NVS initialization failed");
+    return ret;
+  }
 
-  strcpy(stationPtr->ssid_like, wifi_ssid_like);
-  strcpy(stationPtr->device_uuid, device_uuid);
-  strcpy(stationPtr->password, password);
-  stationPtr->device_orientation = orientation;
-  stationPtr->initialized = true;
-  stationPtr->state = s_inactive;
+  // Initialize the ESP-NETIF library (for network interface management)
+  ESP_ERROR_CHECK(esp_netif_init());
 
-  esp_netif_t *sta_netif = esp_netif_create_default_wifi_sta();
-  assert(sta_netif);
+  // Create default event loop
+  ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+  // Initialize Wi-Fi configuration structure
+  wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+  ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+  return ESP_OK;
+}
+
+void device_init(DevicePtr device_ptr, const char *device_uuid, uint8_t device_orientation, const char *wifi_network_prefix, const char *wifi_network_password, uint8_t ap_channel_to_emit, uint8_t ap_max_sta_connections, uint8_t device_is_root, Device_Mode mode) {
+  device_ptr->mode = NAN;
+  device_ptr->state = d_inactive;
+  device_ptr->device_is_root = device_is_root;
+
+  AccessPoint ap = {};
+  device_ptr->access_point = ap;
+  device_ptr->access_point_ptr = &device_ptr->access_point;
+
+  Station station = {};
+  device_ptr->station = station;
+  device_ptr->station_ptr = &device_ptr->station; 
+
+  if (mode == AP) {
+    device_init_ap(device_ptr, ap_channel_to_emit, wifi_network_prefix, device_uuid, wifi_network_password, ap_max_sta_connections);
+  } else if (mode == STATION) {
+    device_init_station(device_ptr, wifi_network_prefix, device_orientation, device_uuid, wifi_network_password);
+  }
 
 }
 
-void station_find_ap(StationPtr stationPtr) {
-  uint16_t number = DEFAULT_SCAN_LIST_SIZE;
-  wifi_ap_record_t ap_info[DEFAULT_SCAN_LIST_SIZE];
-  uint16_t ap_count = 0;
-  memset(ap_info, 0, sizeof(ap_info));
+void device_init_ap(DevicePtr device_ptr, uint8_t channel, const char *wifi_network_prefix ,const char *device_uuid, const char *password, uint8_t max_sta_connections) {
+  // Generate the wifi ssid
+  char wifi_ssid[32];
+  memset(wifi_ssid, 0, sizeof(wifi_ssid));
+  strcpy(wifi_ssid, wifi_network_prefix);
+  strcat(wifi_ssid, "_");
+  strcat(wifi_ssid, device_uuid);
 
-  esp_wifi_scan_start(NULL, true);
-  ESP_LOGI(LOGGING_TAG, "Max AP number ap_info can hold = %u", number);
-  ESP_ERROR_CHECK(esp_wifi_scan_get_ap_num(&ap_count));
-  ESP_ERROR_CHECK(esp_wifi_scan_get_ap_records(&number, ap_info));
-  ESP_LOGI(LOGGING_TAG, "Total APs scanned = %u, actual AP number ap_info holds = %u", ap_count, number);
+  ESP_LOGI(LOGGING_TAG, "Initializing AP with SSID: %s", wifi_ssid);
   
-  uint16_t networks_to_scan;
-  if (ap_count > number) {
-    networks_to_scan = number;
-  } else {
-    networks_to_scan = ap_count;
-  }
-
-  for (int i = 0; i < networks_to_scan; i++) {
-    // Verificamos el prefix de la red
-    if (is_network_allowed(stationPtr->device_uuid, stationPtr->ssid_like, (char*)ap_info[i].ssid)) {
-      uint16_t channel = ap_info[i].primary;
-      if (is_channel_allowed(stationPtr->device_orientation, channel)) {
-        ESP_LOGI(LOGGING_TAG, "SSID \t\t%s", ap_info[i].ssid);
-        ESP_LOGI(LOGGING_TAG, "RSSI \t\t%d", ap_info[i].rssi);
-        ESP_LOGI(LOGGING_TAG, "Channel \t\t%d", ap_info[i].primary);
-        ESP_LOGI(LOGGING_TAG, "Index \t\t%u", i);
-        memcpy(&stationPtr->wifi_ap_found, &ap_info[i], sizeof(ap_info[i]));
-        stationPtr->ap_found = true;
-        break;
-      }
-    }
-  }
-
-  if (!stationPtr->ap_found) {
-    ESP_LOGI(LOGGING_TAG, "No AP found");
-  } else {
-    ESP_LOGI(LOGGING_TAG, "AP found");
-    transform_wifi_ap_record_to_config(stationPtr);
-  }
-}
-
-/*
- * @brief Event handler for WiFi events for station mode
- * @param arg The argument passed during handler registration
- * @param event_base The event base
- * @param event_id The event id
- * @param event_data The event data
- */
-static void event_handler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data) {
-  StationPtr stationPtr = (StationPtr)arg;
-  if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
-    esp_wifi_connect();
-  } else if (event_base == WIFI_EVENT &&
-              event_id == WIFI_EVENT_STA_DISCONNECTED) {
-    if (s_retry_num < EXAMPLE_ESP_MAXIMUM_RETRY) {
-      esp_wifi_connect();
-      s_retry_num++;
-      ESP_LOGI(LOGGING_TAG, "Connection failed, retrying to connect to the AP");
-    } else {
-      ESP_LOGE(LOGGING_TAG, "Failed to connect after %d attempts", s_retry_num);
-      s_retry_num = 0;
-      stationPtr->ap_found = false;
-      stationPtr->state = s_inactive;
-    }
-  } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
-    ip_event_got_ip_t* event = (ip_event_got_ip_t*)event_data;
-    ESP_LOGI(LOGGING_TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
-
-    // Retrieve and print IP address, netmask, and gateway
-    esp_netif_ip_info_t ip_info;
-    esp_netif_get_ip_info(esp_netif_get_handle_from_ifkey("WIFI_STA_DEF"), &ip_info);
-    ESP_LOGI(LOGGING_TAG, "IP Address: " IPSTR, IP2STR(&ip_info.ip));
-    ESP_LOGI(LOGGING_TAG, "Netmask: " IPSTR, IP2STR(&ip_info.netmask));
-    ESP_LOGI(LOGGING_TAG, "Gateway: " IPSTR, IP2STR(&ip_info.gw));
-    s_retry_num = 0;
-    client_create();
-  }
-}
-
-void station_start(StationPtr stationPtr) {
-  ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
-  ESP_ERROR_CHECK(esp_wifi_start());
-}
-
-void station_connect(StationPtr stationPtr) {
-  ESP_LOGI(LOGGING_TAG, "Connecting to %s...", stationPtr->wifi_config.sta.ssid);
-  ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &stationPtr->wifi_config));
-  ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, stationPtr));
-  ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, stationPtr));
-  ESP_ERROR_CHECK(esp_wifi_connect());
-  stationPtr->state = s_active;
-}
-
-void station_disconnect(StationPtr stationPtr) {
-  s_retry_num = EXAMPLE_ESP_MAXIMUM_RETRY + 1;
-  stationPtr->state = s_inactive;
-  ESP_ERROR_CHECK(esp_wifi_disconnect());
-}
-
-void station_restart(StationPtr stationPtr) {
-  station_disconnect(stationPtr);
-  station_start(stationPtr);
-}
-
-bool station_is_initialized(StationPtr stationPtr) {
-  return stationPtr->initialized;
-}
-
-bool station_is_active(StationPtr stationPtr) {
-  return stationPtr->state == s_active;
-}
-
-bool station_found_ap(StationPtr stationPtr) {
-  return stationPtr->ap_found;
+  ap_init(device_ptr->access_point_ptr, channel, wifi_ssid, password, max_sta_connections);
 };
 
-void transform_wifi_ap_record_to_config(StationPtr stationPtr) {
-  memcpy(stationPtr->wifi_config.sta.ssid, stationPtr->wifi_ap_found.ssid, sizeof(stationPtr->wifi_ap_found.ssid));
-  memcpy(stationPtr->wifi_config.sta.bssid, stationPtr->wifi_ap_found.bssid, sizeof(stationPtr->wifi_ap_found.bssid));
-  memcpy(stationPtr->wifi_config.sta.password, stationPtr->password, sizeof(stationPtr->password));
-  stationPtr->wifi_config.sta.bssid_set = true;
+void device_init_station(DevicePtr device_ptr, const char* wifi_ssid_like, uint16_t orientation, char* device_uuid, const char* password) {
+  station_init(device_ptr->station_ptr, wifi_ssid_like, orientation, device_uuid, password);
+};
+
+void device_set_network_ap(DevicePtr device_ptr, const char *network_cidr, const char *network_gateway, const char *network_mask) {
+  ap_set_network(device_ptr->access_point_ptr, network_cidr, network_gateway, network_mask);
+};
+
+void device_reset(DevicePtr device_ptr) {
+  if (device_ptr->state == d_active) {
+    if (device_ptr->mode == AP) {
+      device_stop_ap(device_ptr);
+    } else if (device_ptr->mode == STATION) {
+      device_disconnect_station(device_ptr);
+    } 
+    // else if (device_ptr->mode == ap_station) {
+    //   device_stop_ap_station(device_ptr);
+    // }
+    device_ptr->state = d_inactive;
+  }
+  device_ptr->mode = NAN;
 }
+
+void device_set_mode(DevicePtr device_ptr, Device_Mode mode) {
+  if (device_ptr->mode == mode) {
+    ESP_LOGI(LOGGING_TAG, "Device mode is already setted");
+    return;
+  }
+  device_reset(device_ptr);
+  device_ptr->mode = mode;
+  if (mode == AP) {
+    device_start_ap(device_ptr);
+  } else if (mode == STATION) {
+    device_start_station(device_ptr);
+    device_connect_station(device_ptr);
+  }
+  // else if (mode == ap_station) {
+  //   device_start_ap_station(device_ptr);
+  // }
+}
+
+// AP
+
+void device_start_ap(DevicePtr device_ptr) {
+  ESP_LOGI(LOGGING_TAG, "Starting AP");
+  if (ap_is_initialized(device_ptr->access_point_ptr)) {
+    ap_start(device_ptr->access_point_ptr);
+    device_ptr->state = d_active;
+  } else {
+    ESP_LOGE(LOGGING_TAG, "Access Point is not initialized");
+  }
+};
+
+void device_stop_ap(DevicePtr device_ptr) {
+  // if (device_ptr->state == d_active && device_ptr->mode == ap && ap_is_active(&device_ptr->access_point_ptr)) {
+  // }
+  ap_stop(device_ptr->access_point_ptr);
+};
+
+void device_restart_ap(DevicePtr device_ptr) {
+  ap_restart(device_ptr->access_point_ptr);
+};
+
+// Station
+
+void device_start_station(DevicePtr device_ptr) {
+  if (station_is_initialized(device_ptr->station_ptr)) {
+    station_start(device_ptr->station_ptr);
+    device_ptr->state = d_active;
+  } else {
+    ESP_LOGE(LOGGING_TAG, "Station is not initialized");
+  }
+};
+
+static void device_connect_station_task(void* arg) {
+  DevicePtr device_ptr = (DevicePtr)arg;  // Get the device pointer from the task argument
+
+  while (1) {
+    // If station is disconnected, start scanning for APs
+    if (!station_is_active(device_ptr->station_ptr)) {
+      ESP_LOGI(LOGGING_TAG, "Wi-Fi not connected. Scanning for available networks...");
+      station_find_ap(device_ptr->station_ptr);
+
+      if (station_found_ap(device_ptr->station_ptr)) {
+        ESP_LOGI(LOGGING_TAG, "Wi-Fi found! Attempting to connect.");
+        station_connect(device_ptr->station_ptr);
+      } else {
+        ESP_LOGE(LOGGING_TAG, "No Wi-Fi found. Re-scanning in 10 seconds.");
+      }
+
+      // Wait before trying again
+      vTaskDelay(pdMS_TO_TICKS(10000));  // Scan every 10 seconds
+    }
+
+    vTaskDelay(pdMS_TO_TICKS(10000)); // Wait 10 seconds before checking if station is disconnected
+  }
+
+}
+
+void device_connect_station(DevicePtr device_ptr) {
+  xTaskCreate(device_connect_station_task, "device_connect_station_task", 4096, device_ptr, 5, NULL);
+}
+
+void device_disconnect_station(DevicePtr device_ptr) {
+  station_disconnect(device_ptr->station_ptr);
+};
+
+void device_restart_station(DevicePtr device_ptr) {
+  station_restart(device_ptr->station_ptr);
+};
