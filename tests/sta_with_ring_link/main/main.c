e54afd3d846efd987544a2f541b769ca9d87e973
clrvyntx <73581070+clrvyntx@users.noreply.github.com>
2025-05-06 18:52:22 -0300

    Update main.c

--- tests/sta_with_ring_link/main/main.c
+++ tests/sta_with_ring_link/main/main.c
@@ -19,30 +19,35 @@ void app_main(void) {
     Device device;
     DevicePtr device_ptr = &device;
 
-    const char *wifi_ssid_prefix = "AP_Test_ESP_01";
+    const char *wifi_ssid_prefix = "AP_Test";
     const char *wifi_password = "test123456";
-    const char *device_uuid = "STA";
+    const char *device_uuid = "ESP_02";
     uint16_t device_orientation = 3;
     uint8_t ap_channel_to_emit = 6;
     uint8_t ap_max_sta_connections = 4;
     uint8_t device_is_root = 0;
+    Device_Mode mode = STATION;
 
-    device_init(device_ptr, device_uuid, device_orientation, wifi_ssid_prefix, wifi_password, ap_channel_to_emit, ap_max_sta_connections, device_is_root, STATION);
+    ESP_ERROR_CHECK(wifi_init());
 
     ESP_ERROR_CHECK(ring_link_init());
 
+    device_init(device_ptr, device_uuid, device_orientation, wifi_ssid_prefix, wifi_password, ap_channel_to_emit, ap_max_sta_connections, device_is_root, mode);
     device_start_station(device_ptr);
     device_connect_station(device_ptr);
-    client_create();
 
     const uint8_t message[] = { 0x01, 0x02, 0x03, 0x04, 0x05, 0xFF, 0xAA, 0xBB, 0xCC };
     uint16_t len = sizeof(message) / sizeof(message[0]);
     while (1) {
         bool success = client_send_message(message, len);
-        vTaskDelay(pdMS_TO_TICKS(5000));
-        if(!success){
-            device_connect_station(device_ptr);
+
+        if (!success) {
+            ESP_LOGE("CLIENT", "Error sending message, retrying in 10 seconds...");
+            vTaskDelay(pdMS_TO_TICKS(10000)); // Retry after 10 seconds on failure
+        } else {
+            vTaskDelay(pdMS_TO_TICKS(5000));  // Wait 5 seconds between successful sends
         }
     }
 
 }
+
