8df561e1ede300fb11b049e17fdb1c8184a7c534
clrvyntx <73581070+clrvyntx@users.noreply.github.com>
2025-05-07 04:45:49 -0300

    Update main.c

--- tests/sta_with_ring_link/main/main.c
+++ tests/sta_with_ring_link/main/main.c
@@ -52,11 +52,52 @@ void app_main(void) {
     }
 
     mode = AP;
-    const char *network_cidr = "10.5.6.1";
-    const char *network_gateway = "10.5.6.1";
-    const char *network_mask = "255.255.255.0";
+    char *network_cidr = "10.5.6.1";
+    char *network_gateway = "10.5.6.1";
+    char *network_mask = "255.255.255.0";
 
     device_reset(device_ptr);
+
+    vTaskDelay(pdMS_TO_TICKS(10000));
+
+    device_init(device_ptr, device_uuid, device_orientation, wifi_ssid_prefix, wifi_password, ap_channel_to_emit, ap_max_sta_connections, device_is_root, mode);
+    device_set_network_ap(device_ptr, network_cidr, network_gateway, network_mask);
+    device_start_ap(device_ptr);
+
+    vTaskDelay(pdMS_TO_TICKS(10000));
+
+    mode = STATION;
+
+    device_reset(device_ptr);
+
+    vTaskDelay(pdMS_TO_TICKS(10000));
+
+    device_init(device_ptr, device_uuid, device_orientation, wifi_ssid_prefix, wifi_password, ap_channel_to_emit, ap_max_sta_connections, device_is_root, mode);
+    device_start_station(device_ptr);
+    device_connect_station(device_ptr);
+
+    msgs = 0;
+    while (msgs < 5) {
+        bool success = client_send_message(message, len);
+
+        if (!success) {
+            ESP_LOGE("CLIENT", "Error sending message, retrying in 10 seconds...");
+            vTaskDelay(pdMS_TO_TICKS(10000)); // Retry after 10 seconds on failure
+        } else {
+            vTaskDelay(pdMS_TO_TICKS(5000));  // Wait 5 seconds between successful sends
+            msgs++;
+        }
+    }
+
+    mode = AP;
+    network_cidr = "10.8.22.1";
+    network_gateway = "10.8.22.1";
+    network_mask = "255.255.255.0";
+
+    device_reset(device_ptr);
+
+    vTaskDelay(pdMS_TO_TICKS(10000));
+
     device_init(device_ptr, device_uuid, device_orientation, wifi_ssid_prefix, wifi_password, ap_channel_to_emit, ap_max_sta_connections, device_is_root, mode);
     device_set_network_ap(device_ptr, network_cidr, network_gateway, network_mask);
     device_start_ap(device_ptr);
