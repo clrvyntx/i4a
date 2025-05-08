45ca357a154f47094d6053d4dd386419e3303143
clrvyntx <73581070+clrvyntx@users.noreply.github.com>
2025-05-08 15:04:45 -0300

    Update server.c

--- components/wireless/server/server.c
+++ components/wireless/server/server.c
@@ -9,14 +9,14 @@
 static const char *LOGGING_TAG = "tcp_server";
 
 static int client_sock = -1;
-static int listen_sock = -1;  // Declare listening socket globally
-static bool server_is_up = false;  // Flag to control the server loop
+static bool server_is_up = false;
+static bool is_on_loop = false;
 
 // Function to read data from the client socket
 static void socket_read_loop(const int sock, const char *client_ip) {
   uint8_t rx_buffer[BUFFER_SIZE];
 
-  while (1) {
+  while (is_on_loop) {
     int len = recv(sock, rx_buffer, sizeof(rx_buffer), 0);
     if (len < 0) {
       ESP_LOGE(LOGGING_TAG, "Receive error from %s: errno %d", client_ip, errno);
@@ -44,7 +44,7 @@ static void tcp_server_task(void *pvParameters) {
   dest_addr.sin_port = htons(PORT);
 
   // Create the listening socket
-  listen_sock = socket(addr_family, SOCK_STREAM, ip_protocol);
+  int listen_sock = socket(addr_family, SOCK_STREAM, ip_protocol);
   if (listen_sock < 0) {
     ESP_LOGE(LOGGING_TAG, "Unable to create socket: errno %d", errno);
     vTaskDelete(NULL);
@@ -88,6 +88,7 @@ static void tcp_server_task(void *pvParameters) {
 
     ESP_LOGI(LOGGING_TAG, "Accepted connection from %s", addr_str);
     client_sock = sock;
+    is_on_loop = true;
 
     // Enable TCP Keep-Alive
     int keepAlive = 1;
@@ -104,15 +105,14 @@ static void tcp_server_task(void *pvParameters) {
 
     // Cleanup once connection has been closed
     ESP_LOGI(LOGGING_TAG, "Closing connection from %s", addr_str);
+    is_on_loop = false;
     client_sock = -1;
     shutdown(sock, 0);
     close(sock);
   }
 
-  CLEAN_UP:
-  if (listen_sock >= 0) {
-    close(listen_sock);  // Close listening socket if it's still open
-  }
+  close(listen_sock);
+
   ESP_LOGI(LOGGING_TAG, "Server task is shutting down...");
   vTaskDelete(NULL);
 }
@@ -130,24 +130,12 @@ void server_create() {
 
 void server_close() {
   if (server_is_up) {
-    server_is_up = false;  // Stop accepting new connections
+    server_is_up = false;
 
-    // Ensure cleanup of the client socket if connected
-    if (client_sock >= 0) {
-      ESP_LOGI(LOGGING_TAG, "Closing client connection...");
-      shutdown(client_sock, 0);
-      close(client_sock);
-      client_sock = -1;
+    if (is_on_loop) {
+      is_on_loop = false;
     }
 
-    // If the server is listening, stop listening and close the server socket
-    if (listen_sock >= 0) {
-      ESP_LOGI(LOGGING_TAG, "Closing server socket...");
-      close(listen_sock);
-      listen_sock = -1;
-    }
-
-    ESP_LOGI(LOGGING_TAG, "Server has been closed.");
   } else {
     ESP_LOGW(LOGGING_TAG, "Server is not running, cannot close it.");
   }
