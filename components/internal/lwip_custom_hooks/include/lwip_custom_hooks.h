99a477722c2b74f70cd9d190ca7287c389c445d4
clrvyntx <73581070+clrvyntx@users.noreply.github.com>
2025-08-06 14:32:34 -0300

    Update lwip_custom_hooks.h

--- components/internal/lwip_custom_hooks/include/lwip_custom_hooks.h
+++ components/internal/lwip_custom_hooks/include/lwip_custom_hooks.h
@@ -4,14 +4,10 @@
 
 #ifdef __cplusplus
 extern "C" {
-#endif
+    #endif
 
-#undef LWIP_HOOK_IP4_ROUTE_SRC
-#define LWIP_HOOK_IP4_ROUTE_SRC custom_ip4_route_src_hook
+    struct netif* custom_ip4_route_src_hook(const ip4_addr_t *src, const ip4_addr_t *dest);
 
-struct netif* custom_ip4_route_src_hook(const ip4_addr_t *src, const ip4_addr_t *dest);
-bool get_higher_priority_mask(ip4_addr_t* mask1, ip4_addr_t* mask2);
-
-#ifdef __cplusplus
+    #ifdef __cplusplus
 }
 #endif
