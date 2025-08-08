d1f3649dbd44c725d8110a71ff0ef995a7c6cd3d
clrvyntx <73581070+clrvyntx@users.noreply.github.com>
2025-08-08 05:05:50 -0300

    Update utils.c

--- components/wireless/station/utils.c
+++ components/wireless/station/utils.c
@@ -3,5 +3,20 @@
 #include <stdbool.h>
 
 bool is_network_allowed(char* device_uuid, char* network_prefix, char* network_name) {
-  return ((strstr(network_name, network_prefix) != NULL) && (strstr(network_name, device_uuid) == NULL));
+    // Must contain the prefix
+    if (strstr(network_name, network_prefix) == NULL) {
+        return false;
+    }
+
+    // Must NOT contain the device UUID
+    if (strstr(network_name, device_uuid) != NULL) {
+        return false;
+    }
+
+    // Must NOT be a center node (check if "_C_" is part of the network name)
+    if (strstr(network_name, "_C_") != NULL) {
+        return false;
+    }
+
+    return true;
 }
