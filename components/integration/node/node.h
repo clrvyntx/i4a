eba45036230ce78882c9a10b460b75d6207acc40
clrvyntx <73581070+clrvyntx@users.noreply.github.com>
2025-08-10 23:55:06 -0300

    Update node.h

--- components/integration/node/node.h
+++ components/integration/node/node.h
@@ -1,20 +1,12 @@
 #ifndef _NODE_H_
 #define _NODE_H_
 
-#define NODE_UUID_LEN 7
-
-#include "config.h"
+#include "ring_link.h"
+#include "esp_check.h"
 #include "device.h"
+#include "config.h"
 
-typedef struct node {
-    Device node_device;
-    DevicePtr node_device_ptr;
-    config_orientation_t node_device_orientation;
-    bool node_center_is_root;
-    char node_uuid[NODE_UUID_LEN];
-} node_t;
-
-node_t *node_setup();
+DevicePtr node_setup();
 void node_set_as_ap(uint32_t network, uint32_t mask);
 void node_set_as_sta();
 
