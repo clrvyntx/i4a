bfab7258c8164ee1c1e88f661a67d139b4c31eba
clrvyntx <73581070+clrvyntx@users.noreply.github.com>
2025-08-10 23:17:45 -0300

    Update main.c

--- tests/2_nodes_with_ring_link/main/main.c
+++ tests/2_nodes_with_ring_link/main/main.c
@@ -1,30 +1,24 @@
 #include <stdio.h>
 #include "esp_event.h"
-#include "esp_check.h"
 
-#include "config.h"
-#include "utils.h"
-#include "wifi.h"
-#include "ring_link.h"
-#include "device.h"
+#include "node.h"
 
 #define IS_ROOT 1
 
 static const char *TAG = "==> main";
 
-Device device;
-Device *device_ptr = &device;
+static node_t *current_node;
 
 struct netif *custom_ip4_route_src_hook(const ip4_addr_t *src, const ip4_addr_t *dest) {
     ESP_LOGI(TAG, "Routing hook called for dest: %s", ip4addr_ntoa(dest));
 
-    if (!device_ptr) {
+    if (!current_node->node_device_ptr) {
         ESP_LOGW(TAG, "No device found, falling back to default");
         return (struct netif *)esp_netif_get_netif_impl(get_ring_link_tx_netif());
     }
 
-    uint8_t orientation = device_ptr->device_orientation;
-    uint8_t is_root = device_ptr->device_is_root;
+    uint8_t orientation = current_node->node_device_ptr->device_orientation;
+    uint8_t is_root = current_node->node_device_ptr->device_is_root;
 
     ESP_LOGI(TAG, "Device orientation: %d, is_root: %d", orientation, is_root);
 
@@ -46,7 +40,7 @@ struct netif *custom_ip4_route_src_hook(const ip4_addr_t *src, const ip4_addr_t 
         // Check if dest belongs to my subnet
         if (ip4_addr_netcmp(dest, &my_subnet, &mask)) {
             ESP_LOGI(TAG, "Dest is in my subnet -> route via AP");
-            return (struct netif *)esp_netif_get_netif_impl(device_get_netif(device_ptr));
+            return (struct netif *)esp_netif_get_netif_impl(device_get_netif(current_node->node_device_ptr));
         } else {
             ESP_LOGI(TAG, "Dest is NOT in my subnet -> route via SPI");
             return (struct netif *)esp_netif_get_netif_impl(get_ring_link_tx_netif());
@@ -63,7 +57,7 @@ struct netif *custom_ip4_route_src_hook(const ip4_addr_t *src, const ip4_addr_t 
             return (struct netif *)esp_netif_get_netif_impl(get_ring_link_tx_netif());
         } else {
             ESP_LOGI(TAG, "Dest is other node -> route via AP");
-            return (struct netif *)esp_netif_get_netif_impl(device_get_netif(device_ptr));
+            return (struct netif *)esp_netif_get_netif_impl(device_get_netif(current_node->node_device_ptr));
 
         }
     }
@@ -80,7 +74,7 @@ struct netif *custom_ip4_route_src_hook(const ip4_addr_t *src, const ip4_addr_t 
 
         } else {
             ESP_LOGI(TAG, "Dest is other node -> route via STA");
-	    return (struct netif *)esp_netif_get_netif_impl(device_get_netif(device_ptr));
+	    return (struct netif *)esp_netif_get_netif_impl(device_get_netif(current_node->node_device_ptr));
         }
     }
 
@@ -89,68 +83,30 @@ struct netif *custom_ip4_route_src_hook(const ip4_addr_t *src, const ip4_addr_t 
     return (struct netif *)esp_netif_get_netif_impl(get_ring_link_tx_netif());
 }
 
-void generate_uuid_from_mac(char *uuid_out, size_t len) {
-    uint8_t mac[6];
-    esp_read_mac(mac, ESP_MAC_WIFI_SOFTAP);
-    snprintf(uuid_out, len, "%02X%02X%02X", mac[3], mac[4], mac[5]);
-}
-
 void app_main(void) {
-    config_setup();
-    config_print();
-
-    ESP_ERROR_CHECK(device_wifi_init());
-    ESP_ERROR_CHECK(ring_link_init());
-
-    char device_uuid[7];
-    generate_uuid_from_mac(device_uuid, sizeof(device_uuid));
-
-    uint8_t device_orientation = config_get_orientation();
+    current_node = node_setup();
     uint8_t device_is_root = IS_ROOT;
 
-    char *wifi_network_prefix = "I4A";
-    char *wifi_network_password = "test123456";
-
-    uint8_t ap_channel_to_emit = (rand() % 11) + 1;
-    uint8_t ap_max_sta_connections = 4;
-
-    char *network_cidr;
-    char *network_gateway;
-    char *network_mask = "255.255.0.0";
-
     if(device_orientation == CONFIG_ORIENTATION_CENTER){
-        Device_Mode mode = AP;
-
         if(device_is_root){
-            network_cidr = "10.9.0.1";
-            network_gateway = "10.9.0.1";
+            uint32_t net = 0x0A090000; // 10.9.0.0
+            uint32_t mask = 0xFFFF0000; // 255.255.0.0
+            node_set_as_ap(net, mask);
         } else {
-            network_cidr = "10.8.0.1";
-            network_gateway = "10.8.0.1";
-
+            uint32_t net = 0x0A080000; // 10.8.0.0
+            uint32_t mask = 0xFFFF0000; // 255.255.0.0
+            node_set_as_ap(net, mask);
         }
 
-        device_init(device_ptr, device_uuid, device_orientation, wifi_network_prefix, wifi_network_password, ap_channel_to_emit, ap_max_sta_connections, device_is_root, mode);
-        device_set_network_ap(device_ptr, network_cidr, network_gateway, network_mask);
-        device_start_ap(device_ptr);
     }
 
-    if(device_orientation == CONFIG_ORIENTATION_EAST && device_is_root){
-        Device_Mode mode = AP;
-
-        network_cidr = "10.10.0.1";
-        network_gateway = "10.10.0.2"; // Fake gateway for rerouting to STA
-
-        device_init(device_ptr, device_uuid, device_orientation, wifi_network_prefix, wifi_network_password, ap_channel_to_emit, ap_max_sta_connections, device_is_root, mode);
-        device_set_network_ap(device_ptr, network_cidr, network_gateway, network_mask);
-        device_start_ap(device_ptr);
+    if(current_node->node_device_orientation == CONFIG_ORIENTATION_EAST && device_is_root){
+        uint32_t net = 0x0A0A0000; // 10.10.0.0
+        uint32_t mask = 0xFFFF0000; // 255.255.0.0
+        node_set_as_ap(net, mask);
     }
 
     if(device_orientation == CONFIG_ORIENTATION_WEST && !device_is_root){
-        Device_Mode mode = STATION;
-
-        device_init(device_ptr, device_uuid, device_orientation, wifi_network_prefix, wifi_network_password, ap_channel_to_emit, ap_max_sta_connections, device_is_root, mode);
-        device_start_station(device_ptr);
-        device_connect_station(device_ptr);
+        node_set_as_sta();
     }
 }
