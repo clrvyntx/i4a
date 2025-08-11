3d9dfb1ff29f905acc98fda7985f5a1f7d4e6eb1
clrvyntx <73581070+clrvyntx@users.noreply.github.com>
2025-08-10 22:47:51 -0300

    Update node.h

--- components/integration/node/node.h
+++ components/integration/node/node.h
@@ -1,6 +1,8 @@
 #ifndef _NODE_H_
 #define _NODE_H_
 
+#define NODE_UUID_LEN 7
+
 #include "config.h"
 #include "device.h"
 
@@ -9,11 +11,11 @@ typedef struct node {
     DevicePtr node_device_ptr;
     config_orientation_t node_device_orientation;
     bool node_center_is_root;
-    char node_uuid[7];
+    char node_uuid[NODE_UUID_LEN];
 } node_t;
 
-void node_setup();
-void node_set_as_ap(uint32_t network, uint32_t mask);
-void node_set_as_sta();
+node_t *node_setup();
+void node_set_as_ap(node_t *current_node_ptr, uint32_t network, uint32_t mask);
+void node_set_as_sta(node_t *current_node_ptr);
 
 #endif
