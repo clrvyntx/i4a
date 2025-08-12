d86263bbb9ccda566334ff7355f9e24403b93603
clrvyntx <73581070+clrvyntx@users.noreply.github.com>
2025-08-12 16:04:53 -0300

    Update node.h

--- components/integration/node/node.h
+++ components/integration/node/node.h
@@ -1,10 +1,14 @@
 #ifndef _NODE_H_
 #define _NODE_H_
 
-#include "ring_link.h"
-#include "esp_check.h"
 #include "device.h"
 #include "config.h"
+
+typedef struct node {
+  DevicePtr node_device_ptr;
+  config_orientation_t node_device_orientation;
+  bool node_device_is_center_root;
+} node_t;
 
 DevicePtr node_setup();
 void node_set_as_ap(uint32_t network, uint32_t mask);
