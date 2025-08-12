3a5be1fcb87a97992d178a57ab6325b322fead50
clrvyntx <73581070+clrvyntx@users.noreply.github.com>
2025-08-11 21:46:51 -0300

    Update main.c

--- tests/ap_with_ring_link/main/main.c
+++ tests/ap_with_ring_link/main/main.c
@@ -19,8 +19,32 @@ static DevicePtr device_ptr;
 
 struct netif *custom_ip4_route_src_hook(const ip4_addr_t *src, const ip4_addr_t *dest) {
     ESP_LOGI(TAG, "Routing hook called for dest: %s", ip4addr_ntoa(dest));
-    ESP_LOGI(TAG, "Test, route via AP");
-    return (struct netif *)esp_netif_get_netif_impl(device_get_netif(device_ptr));
+
+    if (!device_ptr) {
+        ESP_LOGW(TAG, "No device found, falling back to default (SPI)");
+        return (struct netif *)esp_netif_get_netif_impl(get_ring_link_tx_netif());
+    }
+
+    esp_netif_t *ap_esp_netif = device_get_netif(device_ptr);
+    if (!ap_esp_netif) {
+        ESP_LOGW(TAG, "AP netif is NULL, falling back to SPI");
+        return (struct netif *)esp_netif_get_netif_impl(get_ring_link_tx_netif());
+    }
+
+    esp_netif_ip_info_t ap_ip_info;
+    if (esp_netif_get_ip_info(ap_esp_netif, &ap_ip_info) != ESP_OK) {
+        ESP_LOGW(TAG, "Failed to get IP info for AP netif, falling back to SPI");
+        return (struct netif *)esp_netif_get_netif_impl(get_ring_link_tx_netif());
+    }
+
+    // Compare destination address with AP subnet
+    if (ip4_addr_netcmp(dest, &ap_ip_info.ip, &ap_ip_info.netmask)) {
+        ESP_LOGI(TAG, "Dest is in the same subnet as AP -> route via AP");
+        return (struct netif *)esp_netif_get_netif_impl(ap_esp_netif);
+    } else {
+        ESP_LOGI(TAG, "Dest is NOT in subnet -> route via SPI");
+        return (struct netif *)esp_netif_get_netif_impl(get_ring_link_tx_netif());
+    }
 }
 
 uint32_t get_subnet_for_orientation(uint8_t orientation) {
