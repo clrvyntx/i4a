f211cef9d478e08d38b59bb1a88e00d2ad8bad54
clrvyntx <73581070+clrvyntx@users.noreply.github.com>
2025-08-12 17:07:05 -0300

    Update node.h

--- components/integration/node/node.h
+++ components/integration/node/node.h
@@ -3,6 +3,7 @@
 
 #include <stdint.h>
 #include <stdbool.h>
+#include "lwip/netif.h"
 
 typedef enum {
     NODE_DEVICE_ORIENTATION_NORTH = 0,
@@ -18,5 +19,7 @@ void node_set_as_sta(void);
 node_device_orientation_t node_get_device_orientation(void);
 bool node_is_device_center_root(void);
 bool node_send_wireless_message(const uint8_t *msg, uint16_t len);
+struct netif *node_get_wifi_netif(void);
+struct netif *node_get_spi_netif(void);
 
 #endif // _NODE_H_
