bfaada728269f5eca749a6086306678ed7b1461d
clrvyntx <73581070+clrvyntx@users.noreply.github.com>
2025-08-13 17:34:02 -0300

    Add files via upload

---
+++ components/routing/wireless/src/wireless.c
@@ -0,0 +1,10 @@
+#include "wireless/wireless.h"
+#include "node.h"
+
+bool wl_send_peer_message(wireless_t *wl, const void *msg, uint16_t len){
+    return node_send_wireless_message(msg, len);
+}
+
+void wl_enable_ap_mode(wireless_t *wl, uint32_t network, uint32_t mask){
+    return node_set_as_ap(network, mask);
+}
