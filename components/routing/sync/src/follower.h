c65e03b525ae222cda34d14b0cdb8ce90704d69c
clrvyntx <73581070+clrvyntx@users.noreply.github.com>
2025-10-30 00:13:19 -0300

    Delete components directory

--- components/routing/sync/src/follower.h
+++
@@ -1,9 +0,0 @@
-#ifndef _I4A_SYNC_FOLLOWER_H_
-#define _I4A_SYNC_FOLLOWER_H_
-
-#include "sync/impl.h"
-#include "sync/sync.h"
-
-sync_impl_t *sync_follower_init(sync_t *sync);
-
-#endif  // _I4A_SYNC_FOLLOWER_H_
--- components/routing/sync/src/leader.h
+++
@@ -1,9 +0,0 @@
-#ifndef _I4A_SYNC_LEADER_H_
-#define _I4A_SYNC_LEADER_H_
-
-#include "sync/impl.h"
-#include "sync/sync.h"
-
-sync_impl_t *sync_leader_init(sync_t *sync);
-
-#endif  // _I4A_SYNC_LEADER_H_
