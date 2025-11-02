bdd1ad1cf0d8906d03f852afa8db472e4070a0ea
clrvyntx <73581070+clrvyntx@users.noreply.github.com>
2025-11-01 23:10:22 -0300

    Update callbacks.c

--- components/integration/callbacks/callbacks.c
+++ components/integration/callbacks/callbacks.c
@@ -37,7 +37,6 @@ static void peer_connected_task(void *arg) {
     peer_event_t event;
     while (1) {
         if (xQueueReceive(peer_connected_queue, &event, portMAX_DELAY)) {
-            ESP_LOGD(TAG, "Peer connected: net=0x%08" PRIx32 ", mask=0x%08" PRIx32, event.net, event.mask);
             wl->callbacks.on_peer_connected(wl->context, event.net, event.mask);
         }
     }
@@ -47,7 +46,6 @@ static void peer_lost_task(void *arg) {
     peer_event_t event;
     while (1) {
         if (xQueueReceive(peer_lost_queue, &event, portMAX_DELAY)) {
-            ESP_LOGD(TAG, "Peer lost: net=0x%08" PRIx32 ", mask=0x%08" PRIx32, event.net, event.mask);
             wl->callbacks.on_peer_lost(wl->context, event.net, event.mask);
         }
     }
@@ -57,8 +55,6 @@ static void peer_message_task(void *arg) {
     message_t m;
     while (1) {
         if (xQueueReceive(peer_message_queue, &m, portMAX_DELAY)) {
-            ESP_LOGD(TAG, "Received wireless message: len=%d", m.length);
-            ESP_LOG_BUFFER_HEXDUMP(TAG, m.data, m.length, ESP_LOG_INFO);
             wl->callbacks.on_peer_message(wl->context, m.data, m.length);
         }
     }
@@ -68,8 +64,6 @@ static void sibling_message_task(void *arg) {
     message_t m;
     while (1) {
         if (xQueueReceive(sibling_message_queue, &m, portMAX_DELAY)) {
-            ESP_LOGD(TAG, "Received sibling message: len=%d", m.length);
-            ESP_LOG_BUFFER_HEXDUMP(TAG, m.data, m.length, ESP_LOG_INFO);
             sb->callback(sb->context, m.data, m.length);
         }
     }
@@ -176,6 +170,7 @@ wireless_t *node_get_wireless_instance(void){
 siblings_t *node_get_siblings_instance(void){
     return sb;
 }
+
 
 
 
