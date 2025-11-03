8651625de88f4224e0678238a61f53a8062fbbc4
clrvyntx <73581070+clrvyntx@users.noreply.github.com>
2025-11-03 00:36:02 -0300

    Update main.c

--- tests/2_nodes_with_ring_link/main/main.c
+++ tests/2_nodes_with_ring_link/main/main.c
@@ -20,15 +20,13 @@ static uint32_t l_mask   = 0xFFE00000; // 255.224.0.0 (/11)
 static uint32_t c_subnet = 0x0A600000; // 10.96.0.0
 static uint32_t c_mask   = 0xFFFC0000; // 255.252.0.0 (/14)
 
-static uint32_t o_subnet = 0x0B000000; // 11.0.0.0
-static uint32_t o_mask = 0xFF000000; // 255.0.0.0
+static uint8_t orientation;
+static uint8_t is_root;
 
 // Custom routing hook
 struct netif *custom_ip4_route_src_hook(const ip4_addr_t *src, const ip4_addr_t *dest) {
     uint32_t src_ip = lwip_ntohl(ip4_addr_get_u32(src));
     uint32_t dst_ip = lwip_ntohl(ip4_addr_get_u32(dest));
-    uint8_t orientation = node_get_device_orientation();
-    uint8_t is_root = node_is_device_center_root();
 
     // === Case 1: Center ===
     if (orientation == NODE_DEVICE_ORIENTATION_CENTER) {
@@ -39,7 +37,7 @@ struct netif *custom_ip4_route_src_hook(const ip4_addr_t *src, const ip4_addr_t 
                 return (struct netif *)esp_netif_get_netif_impl(node_get_wifi_netif());
             }
         } else {
-            if ((dst_ip & c_mask) != c_subnet) {
+            if ((dst_ip & r_mask) != r_subnet) {
                 return (struct netif *)esp_netif_get_netif_impl(node_get_spi_netif());
             } else {
                 return (struct netif *)esp_netif_get_netif_impl(node_get_wifi_netif());
@@ -52,7 +50,7 @@ struct netif *custom_ip4_route_src_hook(const ip4_addr_t *src, const ip4_addr_t 
         if (node_is_point_to_point_message(dst_ip)) {
             return (struct netif *)esp_netif_get_netif_impl(node_get_wifi_netif());
         }
-        if ((dst_ip & c_mask) != c_subnet) {
+        if ((dst_ip & r_mask) != r_subnet) {
             return (struct netif *)esp_netif_get_netif_impl(node_get_spi_netif());
         } else {
             return (struct netif *)esp_netif_get_netif_impl(node_get_wifi_netif());
@@ -64,7 +62,7 @@ struct netif *custom_ip4_route_src_hook(const ip4_addr_t *src, const ip4_addr_t 
         if (node_is_point_to_point_message(dst_ip)) {
             return (struct netif *)esp_netif_get_netif_impl(node_get_wifi_netif());
         }
-        if ((dst_ip & c_mask) == c_subnet) {
+        if ((dst_ip & r_mask) == r_subnet) {
             return (struct netif *)esp_netif_get_netif_impl(node_get_spi_netif());
         } else {
             return (struct netif *)esp_netif_get_netif_impl(node_get_wifi_netif());
@@ -75,36 +73,33 @@ struct netif *custom_ip4_route_src_hook(const ip4_addr_t *src, const ip4_addr_t 
     return (struct netif *)esp_netif_get_netif_impl(node_get_spi_netif());
 }
 
-
 void app_main(void) {
     node_setup();
 
-    node_device_orientation_t orientation = node_get_device_orientation();
-    bool device_is_root = node_is_device_center_root();
+    orientation = node_get_device_orientation();
+    is_root = node_is_device_center_root();
 
     siblings_t *sb = node_get_siblings_instance();
     rs_init(&rs, sb);
     cm_init(&rs, orientation);
 
     if(orientation == NODE_DEVICE_ORIENTATION_CENTER){
-        if(device_is_root){
-            node_set_as_ap(o_subnet, o_mask);
+        if(is_root){
+            node_set_as_ap(r_subnet, r_mask);
         } else {
             node_set_as_ap(c_subnet, c_mask);
         }
     }
 
-    if(orientation == NODE_DEVICE_ORIENTATION_NORTH && device_is_root){
+    if(orientation == NODE_DEVICE_ORIENTATION_NORTH && is_root) {
         node_set_as_ap(l_subnet, l_mask);
     }
 
-    if(orientation == NODE_DEVICE_ORIENTATION_WEST && !device_is_root){
+    if(orientation == NODE_DEVICE_ORIENTATION_WEST && !is_root) {
         node_set_as_sta();
     }
 
     while(1) {
         vTaskDelay(pdMS_TO_TICKS(10000));
-        uint8_t my_channel = cm_get_suggested_channel();
-        ESP_LOGI(TAG, "Got suggested channel: %u", my_channel);
     }
 }
