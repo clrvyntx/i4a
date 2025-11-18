a4284fb1160fae3cd5096fb85af3a512fcf6d85c
clrvyntx <73581070+clrvyntx@users.noreply.github.com>
2025-11-18 00:45:06 -0300

    Update station.c

--- components/wireless/station/station.c
+++ components/wireless/station/station.c
@@ -105,7 +105,11 @@ static void event_handler(void* arg, esp_event_base_t event_base, int32_t event_
     switch (event_id) {
       case WIFI_EVENT_STA_CONNECTED:
         if (!stationPtr->is_apsta) {
-            cm_provide_to_siblings(stationPtr->wifi_ap_found.primary);
+          int retry = 0;
+          while(!cm_provide_to_siblings(stationPtr->wifi_ap_found.primary) && retry < MAX_RETRIES){
+            retry++;
+            vTaskDelay(pdMS_TO_TICKS(100));
+          }
         }
 
         if(!stationPtr->is_fully_connected){
