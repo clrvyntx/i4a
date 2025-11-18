e57bf7bb4dcb8258037e979edc8d4e71608bac06
clrvyntx <73581070+clrvyntx@users.noreply.github.com>
2025-11-17 23:40:31 -0300

    Update routing.c

--- components/routing/routing/src/routing.c
+++ components/routing/routing/src/routing.c
@@ -12,8 +12,6 @@
 #include "sync/sync.h"
 #include "wireless/wireless.h"
 
-#define NODE_STARTUP_DELAY_SECONDS 10
-
 static void on_sibling_message(void *ctx, const uint8_t *raw_event, uint16_t len) {
     routing_t *self = ctx;
     rt_internal_queue_t *queue = &self->internal_queue;
@@ -221,9 +219,6 @@ void rt_on_start(routing_t *self) {
         self->deps.rs, RS_ROUTING, (ring_callback_t){ .callback = on_sibling_message, .context = self }
     );
 
-    // Wait for all devices of the node to setup
-    os_delay_ms(NODE_STARTUP_DELAY_SECONDS * 1000);
-
     // Request the first critical section
     sync_request_critical_section(self->deps.sync, RS_ROUTING);
 }
@@ -248,3 +243,4 @@ void rt_on_tick(routing_t *self, uint32_t dt_ms) {
 void rt_destroy(routing_t *self) {
     mutex_destroy(self->node_state.m_lock);
 }
+
