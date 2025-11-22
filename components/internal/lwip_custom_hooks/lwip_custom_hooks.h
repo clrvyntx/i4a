06bdea2ae8ef2b68eb424071606a76d81e3c2b8f
clrvyntx <73581070+clrvyntx@users.noreply.github.com>
2025-11-22 03:14:26 -0300

    Update lwip_custom_hooks.h

--- components/internal/lwip_custom_hooks/lwip_custom_hooks.h
+++ components/internal/lwip_custom_hooks/lwip_custom_hooks.h
@@ -1,10 +1,17 @@
 #pragma once
 
 #include "lwip/netif.h"
+#include "lwip/opt.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+#undef PBUF_POOL_BUFSIZE
+#define PBUF_POOL_BUFSIZE 1700
+
+#undef PBUF_POOL_SIZE
+#define PBUF_POOL_SIZE 50
 
 #undef LWIP_HOOK_IP4_ROUTE_SRC
 #define LWIP_HOOK_IP4_ROUTE_SRC custom_ip4_route_src_hook
