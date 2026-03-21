0253d7cc3b43d28fd26602dea49bde6254c4f58f
clrvyntx <73581070+clrvyntx@users.noreply.github.com>
2026-03-21 04:15:04 -0300

    Update routing_hooks.h

--- components/integration/routing_hooks/routing_hooks.h
+++ components/integration/routing_hooks/routing_hooks.h
@@ -1,6 +1,7 @@
 #ifndef _ROUTING_HOOKS_H_
 #define _ROUTING_HOOKS_H_
 
+#include <stdint.h> 
 #include "esp_netif_net_stack.h"
 #include "esp_netif.h"
 #include "routing/routing.h"
@@ -10,10 +11,15 @@ typedef enum routing_hook_type {
     ROUTING_HOOK_FORWARDER,
     ROUTING_HOOK_HOME,
     ROUTING_HOOK_ROOT_FORWARDER,
+    ROUTING_HOOK_CUSTOM,
     ROUTING_HOOK_COUNT
 } routing_hook_type_t;
 
+// Function pointer type for custom routing hooks
+typedef struct netif *(*routing_hook_func_t)(uint32_t src_ip, uint32_t dst_ip);
+
 void node_set_routing_hook(routing_hook_type_t hook);
+void node_register_custom_routing_hook(routing_hook_func_t hook);
 struct netif *node_do_routing(uint32_t src, uint32_t dst);
 routing_t *node_get_rt_instance(void);
 
